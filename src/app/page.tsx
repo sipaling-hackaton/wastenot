034280197f6b2f85c18c2827db0abc3cd010a469
Aldrik56 <aldrik.ciaputra@student.umn.ac.id>
2024-07-13 00:25:16 +0700

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,45 @@ import 'swiper/css';
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center" style={{backgroundImage:"url('./image/bg-home.png')",backgroundSize:"cover"}}>
-      <div className="profileCard">
-        <div className="">
-          <p>Hello</p>
-          <p>Welcome To</p>
+    <main className="flex min-h-screen flex-col items-center" style={{backgroundImage:"url('./image/bg-home.png')",backgroundSize:"contain",backgroundRepeat:"no-repeat"}}>
+      <div className="profileCard salamContainer">
+        <div >
+          <p className="grayText">Hello</p>
+          <p className="bigText">Welcome To</p>
         </div>
         <img className="logo" src="./image/logo.png" alt="" />
       </div>
-      <div className="profileCard cardHijau">
-        <div className="profileAtas">
-          <img src="./image/dokter.png" alt="" />
-          <div>
-            <p className="">Aldrik Ciaputra</p>
-            <p className="pangkat">Raja</p>
-          </div>
-          <div>
-            <img src="./image/arrow-right.png" alt="" />
-          </div>
-        </div>
-        <hr className="hrCustom"/>
-        <div className="profileAtas">
+      <div className="cardHijau">
+        <div className="profileCardUser ">
           <div className="profileAtas">
-            <img src="./image/calendarIcon.png" alt="" />
-            <p>Rp 100.000</p>
+            <div className="namaDanPhoto">
+              <img src="./image/dokter.png" alt="" />
+              <div>
+                <p className="nama">Aldrik Ciaputra</p>
+                <p className="pangkat">Raja</p>
+              </div>
+            </div>
+            <div>
+              <img src="./image/arrow-right.png" alt="" />
+            </div>
           </div>
+          <hr className="hrCustom"/>
           <div className="profileAtas">
-            <img src="./image/amount.png" alt="" />
-            <p>1000 kg</p>
+            <div className="profileAtas">
+              <img src="./image/calendarIcon.png" alt="" />
+              <p className="text">Rp 100.000</p>
+            </div>
+            <div className="profileAtas">
+              <img src="./image/amount.png" alt="" />
+              <p className="text">1000 kg</p>
+            </div>
           </div>
         </div>
       </div>
-      <img src="./image/bannerEvent.png" alt="" />
-      <img src="./image/mapBank sampah.png" alt="" />
+      <div className="section2">
+        <img src="./image/bannerEvent.png" alt="" />
+        <div style={{width: "100%",height:"200px",paddingBottom:"20px"}}><iframe width="100%" height="300"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Jl%20Scientia%20Square%20Barat%201+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe></div>
+      </div>
     </main>
   );
 }
